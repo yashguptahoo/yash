5f9201912343e33993d099b6d78fa02a67ece6ca
yashguptahoo <74765371+yashguptahoo@users.noreply.github.com>
2024-08-15 06:47:15 +0000

    feat: initiated the project

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
-  const contract = await hre.ethers.deployContract("TestNFT");
+  const contract = await hre.ethers.deployContract("TestToken");
   await contract.waitForDeployment();
   const deployedContract = await contract.getAddress();
   fs.writeFileSync("contract.txt", deployedContract);
